ed4834d7ba1ba5e969576c89e36ed1aa63e2f3e1
McJty <mcjty1@gmail.com>
2020-10-24 15:32:46 +0200

    First commit

---
+++ src/TpTools.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Vintagestory.API.Common;
+
+[assembly: ModInfo("fancytools",
+    Description = "Fancy Tools",
+    Authors = new[] { "McJty" })]
+
+
+namespace FancyTools
+{
+
+    public class FancyTools : ModSystem {
+
+        public static string MODID = "fancytools";
+
+        public Dictionary<string, SeedBagInventory> seedBagInventories = new Dictionary<string, SeedBagInventory>();
+
+        public override void Start(ICoreAPI api) {
+            base.Start(api);
+			// api.RegisterBlockBehaviorClass(TestBehavior.NAME, typeof(TestBehavior));
+            // api.RegisterBlockEntityClass("FancyMold", typeof(BlockEntityFancyMold));
+
+            api.RegisterItemClass(SeedBagItem.NAME, typeof(SeedBagItem));
+            api.RegisterItemClass(SpadeItem.NAME, typeof(SpadeItem));
+        }
+    }
+    
+}
